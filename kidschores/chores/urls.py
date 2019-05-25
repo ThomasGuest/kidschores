c08713878d7d07cfe77c959d3a0a4b10a0cf7af9
ThomasGuest <thomasguest86@outlook.com>
2019-05-24 20:56:49 -0400

    chores list view and template

--- kidschores/chores/urls.py
+++ kidschores/chores/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'chores'
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('chore_list/', views.chore_list, name='chore_list')
 
 ]
--- kidschores/chores/views.py
+++ kidschores/chores/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Chores
 
 
 # Create your views here.
@@ -6,3 +7,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'chores/home.html')
+
+
+def chore_list(request):
+    """Show list of chores"""
+    chores = Chores.objects.order_by('name')
+    context = {'chores' : chores}
+    return render(request, 'chores/chore_list.html', context)
