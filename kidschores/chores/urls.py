a666bc883448136b551787fd3429cc7c7348b762
ThomasGuest <thomasguest86@outlook.com>
2019-05-31 18:32:09 -0400

    user registration

--- kidschores/chores/urls.py
+++ kidschores/chores/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 
 app_name = 'chores'
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('chore_list/', views.chore_list, name='chore_list')
+    path('chore_list/', views.chore_list, name='chore_list'),
+    path('register/', views.register, name='register')
 
 ]
--- kidschores/chores/views.py
+++ kidschores/chores/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Chores
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout, authenticate, login
 
 
 # Create your views here.
@@ -14,3 +16,26 @@ def chore_list(request):
     chores = Chores.objects.order_by('name')
     context = {'chores' : chores}
     return render(request, 'chores/chore_list.html', context)
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            login(request, user)
+            return redirect("chores:home")
+        
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+
+            return render(request = request,
+                        template_name = "chores/register.html",
+                        context={"form":form}
+            )
+
+    form = UserCreationForm        
+    return render(request = request,
+                template_name = "chores/register.html",
+                context={"form":form})
